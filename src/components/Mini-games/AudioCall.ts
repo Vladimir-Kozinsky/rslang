8ff35295cc0f9ee516a858176228ec7f77c70e9b
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-08-29 01:52:26 +0500

    feat: create method which creates words to the game

--- src/components/Mini-games/AudioCall.ts
+++ src/components/Mini-games/AudioCall.ts
@@ -59,6 +59,51 @@ class AudioCall {
         audioCallContainer.append(optionsContainer)
     }
 
+    async createWordsForGame(difficulty: string) {
+      const api = new GamesApi();
+      const randomPage = Math.floor(Math.random() * 30).toString();
+      const wordsArr: Word[] = await api.getWords(difficulty, randomPage);
+      
+      function shuffleArr(array: Word[]) {
+        let currentIndex = array.length;
+        let randomIndex;
+        while (currentIndex !== 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+  
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+          ];
+        }
+  
+        return array;
+      }
+      shuffleArr(wordsArr);
+      const dataForCart = [];
+     for (let i = 0; i < wordsArr.length; i += 1) {
+      const {word} = wordsArr[i];
+      const wordAudio = `https://react-learnwords-shahzod.herokuapp.com/${wordsArr[i].audio}`
+      const {wordTranslate} = wordsArr[i];
+      const wrongTranslates: string[] = [];
+      let wrongTranslatesAmount: number = 4;
+      const wordImage = wordsArr[i].image;
+      dataForCart.push({
+        word,
+        wordTranslate,
+        wordTranslates: [wrongTranslates, wordTranslate],
+        wordImage,
+        wordAudio
+      });
+      for (let j = 0; j < wrongTranslatesAmount; j += 1) {
+        const randomIndex = Math.floor(Math.random() * 19);
+        if(wrongTranslates.includes(wordsArr[randomIndex].wordTranslate) || randomIndex === i) wrongTranslatesAmount += 1;
+        if(randomIndex !== i && !wrongTranslates.includes(wordsArr[randomIndex].wordTranslate)) wrongTranslates.push(wordsArr[randomIndex].wordTranslate);
+      }
+     }
+     return dataForCart
+  }
+
 }
 
 export default AudioCall;
