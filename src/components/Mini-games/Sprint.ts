7a92ca906b56f0bcead30bfe9dec2c237049021d
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-08-29 02:00:53 +0500

    fix: fix button which returns client to the previous page

--- src/components/Mini-games/Sprint.ts
+++ src/components/Mini-games/Sprint.ts
@@ -6,19 +6,19 @@ import MiniGames from './MiniGames';
 
 class Sprint {
   createPage() {
-    const content = document.querySelector(
-      '.container__content'
-    ) as HTMLDivElement;
+    const miniGames = new MiniGames();
+    const content = document.querySelector('.container__content') as HTMLDivElement;
 
     const sprintContainer = document.createElement('div') as HTMLDivElement;
     sprintContainer.classList.add('sprint__container');
 
+    const returnToPrevPage = document.createElement('div');
+    returnToPrevPage.classList.add('return-to-prev-page');
+
     const title = document.createElement('h2') as HTMLHeadingElement;
     title.textContent = 'Спринт';
 
-    const sprintstatisticsContainer = document.createElement(
-      'div'
-    ) as HTMLDivElement;
+    const sprintstatisticsContainer = document.createElement('div') as HTMLDivElement;
     sprintstatisticsContainer.classList.add('sprint-statistics__container');
 
     const points = document.createElement('span') as HTMLSpanElement;
@@ -38,14 +38,10 @@ class Sprint {
     const wordAudio = document.createElement('audio') as HTMLAudioElement;
     wordAudio.classList.add('word-audio');
 
-    const wordAudioExample = document.createElement(
-      'audio'
-    ) as HTMLAudioElement;
+    const wordAudioExample = document.createElement('audio') as HTMLAudioElement;
     wordAudioExample.classList.add('word-audio-example');
 
-    const currentWordContainer = document.createElement(
-      'div'
-    ) as HTMLDivElement;
+    const currentWordContainer = document.createElement('div') as HTMLDivElement;
     currentWordContainer.classList.add('sprint-current-word__container');
 
     for (let i = 0; i < 4; i += 1) {
@@ -74,8 +70,13 @@ class Sprint {
     trueBtn.textContent = 'Верно';
     trueBtn.classList.add('sprint-button__true');
 
+    returnToPrevPage.addEventListener('click', () => {
+      miniGames.backToPage();
+    });
+
     document.querySelector('aside')?.remove();
     content.append(sprintContainer);
+    sprintContainer.append(returnToPrevPage);
     sprintContainer.append(title);
     sprintContainer.append(sprintstatisticsContainer);
     sprintContainer.append(cart);
@@ -104,10 +105,7 @@ class Sprint {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        [array[currentIndex], array[randomIndex]] = [
-          array[randomIndex],
-          array[currentIndex],
-        ];
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
       }
 
       return array;
@@ -141,28 +139,19 @@ class Sprint {
   }
 
   async appendWordsToPage(difficulty: string) {
-    const wordContainer = document.querySelector(
-      '.sprint-current-word__container'
-    );
+    const wordContainer = document.querySelector('.sprint-current-word__container');
     const word = document.querySelector('.sprint-word') as HTMLHeadingElement;
-    const translatedWord = document.querySelector(
-      '.sprint-translated-word'
-    ) as HTMLHeadingElement;
+    const translatedWord = document.querySelector('.sprint-translated-word') as HTMLHeadingElement;
     const wordAudio = document.querySelector('.word-audio');
     const wordAudioExample = document.querySelector('.word-audio-example');
-    const points = document.querySelector(
-      '.sprint-statistics-point'
-    ) as HTMLSpanElement;
-    const time = document.querySelector(
-      '.sprint-statistics-time'
-    ) as HTMLSpanElement;
+    const points = document.querySelector('.sprint-statistics-point') as HTMLSpanElement;
+    const time = document.querySelector('.sprint-statistics-time') as HTMLSpanElement;
     const createdWords = await this.createWordsForGame(difficulty);
     const { clonedArr } = createdWords;
     const { wordsArr } = createdWords;
-    const correctAnswers: { word: string; wordTranslate: string }[] = [];
-    const inCorrectAnswers: { word: string; wordTranslate: string }[] = [];
-    const wrongTranslatedWordIndexes: number[] =
-      createdWords.wrongTranslatedWordsIndexes;
+    const correctAnswers: { word: string; wordTranslate: string; wordAudio: string; }[] = [];
+    const inCorrectAnswers: { word: string; wordTranslate: string; wordAudio: string; }[] = [];
+    const wrongTranslatedWordIndexes: number[] = createdWords.wrongTranslatedWordsIndexes;
     let currentIndex: number = 0;
     const currentWord: string = clonedArr[currentIndex].word;
     word.textContent = currentWord;
@@ -178,11 +167,7 @@ class Sprint {
       time.textContent = (+time.textContent! - 1).toString();
       if (time.textContent === '0') {
         clearInterval(timeToStop);
-        this.createResultsPage(
-          +points.textContent!,
-          correctAnswers,
-          inCorrectAnswers
-        );
+        this.createResultsPage(+points.textContent!, correctAnswers, inCorrectAnswers);
       }
     }, 1000);
     timeToStop;
@@ -200,11 +185,7 @@ class Sprint {
       const target = e.target as HTMLButtonElement;
       if (currentIndex > 18) {
         clearInterval(timeToStop);
-        this.createResultsPage(
-          +points.textContent!,
-          correctAnswers,
-          inCorrectAnswers
-        );
+        this.createResultsPage(+points.textContent!, correctAnswers, inCorrectAnswers);
       }
       if (target.classList.contains('sprint-button__true')) {
         if (wrongTranslatedWordIndexes.includes(currentIndex)) {
@@ -214,11 +195,13 @@ class Sprint {
           inCorrectAnswers.push({
             word: word.textContent!,
             wordTranslate: findTranslate(word.textContent!)!,
+            wordAudio: clonedArr[currentIndex].audio,
           });
         } else {
           correctAnswers.push({
             word: word.textContent!,
             wordTranslate: translatedWord.textContent!,
+            wordAudio: clonedArr[currentIndex].audio,
           });
           currentIndex += 1;
           points.textContent = (+points.textContent! + 20).toString();
@@ -230,6 +213,7 @@ class Sprint {
           correctAnswers.push({
             word: word.textContent!,
             wordTranslate: findTranslate(word.textContent!)!,
+            wordAudio: clonedArr[currentIndex].audio,
           });
           currentIndex += 1;
           points.textContent = (+points.textContent! + 20).toString();
@@ -239,6 +223,7 @@ class Sprint {
           inCorrectAnswers.push({
             word: word.textContent!,
             wordTranslate: translatedWord.textContent!,
+            wordAudio: clonedArr[currentIndex].audio,
           });
           currentIndex += 1;
           word.textContent = clonedArr[currentIndex].word;
@@ -252,11 +237,7 @@ class Sprint {
       const keyName = e.key;
       if (currentIndex > 18) {
         clearInterval(timeToStop);
-        this.createResultsPage(
-          +points.textContent!,
-          correctAnswers,
-          inCorrectAnswers
-        );
+        this.createResultsPage(+points.textContent!, correctAnswers, inCorrectAnswers);
       }
       // eslint-disable-next-line default-case
       switch (keyName) {
@@ -268,11 +249,13 @@ class Sprint {
             inCorrectAnswers.push({
               word: word.textContent!,
               wordTranslate: findTranslate(word.textContent!)!,
+              wordAudio: clonedArr[currentIndex].audio,
             });
           } else {
             correctAnswers.push({
               word: word.textContent!,
               wordTranslate: translatedWord.textContent!,
+              wordAudio: clonedArr[currentIndex].audio,
             });
             currentIndex += 1;
             points.textContent = (+points.textContent! + 20).toString();
@@ -285,6 +268,7 @@ class Sprint {
             correctAnswers.push({
               word: word.textContent!,
               wordTranslate: findTranslate(word.textContent!)!,
+              wordAudio: clonedArr[currentIndex].audio,
             });
             currentIndex += 1;
             points.textContent = (+points.textContent! + 20).toString();
@@ -294,29 +278,24 @@ class Sprint {
             inCorrectAnswers.push({
               word: word.textContent!,
               wordTranslate: translatedWord.textContent!,
+              wordAudio: clonedArr[currentIndex].audio,
             });
             currentIndex += 1;
             word.textContent = clonedArr[currentIndex].word;
             translatedWord.textContent = clonedArr[currentIndex].wordTranslate;
           }
-    }
-  
-
+      }
     });
-
   }
 
   createResultsPage(
     points: number,
-    correctAnswers: { word: string; wordTranslate: string }[],
-    inCorrectAnswers: { word: string; wordTranslate: string }[]
+    correctAnswers: { word: string; wordTranslate: string; wordAudio: string }[],
+    inCorrectAnswers: { word: string; wordTranslate: string; wordAudio: string }[]
   ) {
-    const content = document.querySelector(
-      '.container__content'
-    ) as HTMLDivElement;
-    const containerBlock = document.querySelector(
-      '.container'
-    ) as HTMLDivElement;
+    console.log(inCorrectAnswers);
+    const content = document.querySelector('.container__content') as HTMLDivElement;
+    const containerBlock = document.querySelector('.container') as HTMLDivElement;
     const app = new App();
     const container = new Container();
     containerBlock.prepend(container.createMenu());
@@ -324,9 +303,11 @@ class Sprint {
 
     const resultsCartContainer = document.createElement('div');
     resultsCartContainer.classList.add('sprint-results-cart__container');
+    resultsCartContainer.classList.add('results-cart__container');
 
     const resultsCart = document.createElement('div');
     resultsCart.classList.add('sprint-results-cart');
+    resultsCart.classList.add('results-cart');
 
     const title = document.createElement('h2') as HTMLHeadingElement;
     title.textContent = 'Ваш результат:';
@@ -337,59 +318,58 @@ class Sprint {
 
     const answersBlock = document.createElement('div') as HTMLDivElement;
     answersBlock.classList.add('sprint-results-answers-block');
+    answersBlock.classList.add('results-answers-block');
 
-    const correctAnswersContainer = document.createElement(
-      'div'
-    ) as HTMLDivElement;
+    // audio plays when clicking to the icon
+    answersBlock.addEventListener('click', (e: Event) => {
+      const target = e.target as HTMLDivElement;
+      if(target.classList.contains('correct-answer__audio') || target.classList.contains('inCorrect-answer__audio')) {
+        const audio = target.children[0] as HTMLAudioElement;
+        audio.play();
+      }
+    })    
+    const correctAnswersContainer = document.createElement('div') as HTMLDivElement;
 
-    const correctAnswersTotal = document.createElement(
-      'span'
-    ) as HTMLSpanElement;
+    const correctAnswersTotal = document.createElement('span') as HTMLSpanElement;
     correctAnswersTotal.classList.add('sprint-results-correct-answers-total');
+    correctAnswersTotal.classList.add('results-correct-answers-total');
     correctAnswersTotal.textContent = `Правильные ответы:${correctAnswers.length.toString()}`;
 
     for (let i = 0; i < correctAnswers.length; i += 1) {
-      const correctAnswerBlock = document.createElement(
-        'div'
-      ) as HTMLDivElement;
+      const correctAnswerBlock = document.createElement('div') as HTMLDivElement;
       correctAnswerBlock.classList.add('correct-answer__block');
-      const answerAudio = document.createElement('audio') as HTMLAudioElement;
+      const answerAudio = document.createElement('div') as HTMLDivElement;
       answerAudio.classList.add('correct-answer__audio');
-      const correctAnswerWord = document.createElement(
-        'span'
-      ) as HTMLSpanElement;
-      correctAnswerWord.classList.add('correct-answer__word');
+      const correctAnswerWord = document.createElement('span') as HTMLSpanElement;
 
+      correctAnswerWord.classList.add('correct-answer__word');
+      answerAudio.innerHTML = `<audio>
+      <source src="https://react-learnwords-shahzod.herokuapp.com/${correctAnswers[i].wordAudio}" type="">
+    </audio>`;
       correctAnswerWord.textContent = `${correctAnswers[i].word} - ${correctAnswers[i].wordTranslate}`;
       correctAnswersContainer.append(correctAnswerBlock);
       correctAnswerBlock.append(answerAudio);
       correctAnswerBlock.append(correctAnswerWord);
     }
 
-    const inCorrectAnswersContainer = document.createElement(
-      'div'
-    ) as HTMLDivElement;
+    const inCorrectAnswersContainer = document.createElement('div') as HTMLDivElement;
 
-    const inCorrectAnswersTotal = document.createElement(
-      'span'
-    ) as HTMLSpanElement;
-    inCorrectAnswersTotal.classList.add(
-      'sprint-results-incorrect-answers-total'
-    );
+    const inCorrectAnswersTotal = document.createElement('span') as HTMLSpanElement;
+    inCorrectAnswersTotal.classList.add('sprint-results-incorrect-answers-total');
+    inCorrectAnswersTotal.classList.add('results-incorrect-answers-total');
     inCorrectAnswersTotal.textContent = `Неправильные ответы:${inCorrectAnswers.length}`;
 
     for (let i = 0; i < inCorrectAnswers.length; i += 1) {
-      const inCorrectAnswerBlock = document.createElement(
-        'div'
-      ) as HTMLDivElement;
+      const inCorrectAnswerBlock = document.createElement('div') as HTMLDivElement;
       inCorrectAnswerBlock.classList.add('inCorrect-answer__block');
-      const answerAudio = document.createElement('audio') as HTMLAudioElement;
+      const answerAudio = document.createElement('div') as HTMLDivElement;
       answerAudio.classList.add('inCorrect-answer__audio');
-      const inCorrectAnswerWord = document.createElement(
-        'span'
-      ) as HTMLSpanElement;
+      const inCorrectAnswerWord = document.createElement('span') as HTMLSpanElement;
       inCorrectAnswerWord.classList.add('inCorrect-answer__word');
 
+      answerAudio.innerHTML = `<audio>
+        <source src="https://react-learnwords-shahzod.herokuapp.com/${inCorrectAnswers[i].wordAudio}" type="">
+      </audio>`;
       inCorrectAnswerWord.textContent = `${inCorrectAnswers[i].word} - ${inCorrectAnswers[i].wordTranslate}`;
       inCorrectAnswersContainer.append(inCorrectAnswerBlock);
       inCorrectAnswerBlock.append(answerAudio);
