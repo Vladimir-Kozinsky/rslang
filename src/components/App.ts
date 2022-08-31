ada495fe9f2ee5096448f9f07b5ec789153fe4a6
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-08-31 11:34:08 +0400

    fix: page-switcher

--- src/components/App.ts
+++ src/components/App.ts
@@ -4,15 +4,18 @@ import Sprint from './Mini-games/Sprint';
 import MainPageController from './MainPage/MainPageController';
 import AuthController from './Auth/AuthController';
 import ApiData from './Api/ApiData';
+import Ebook from './Ebook/Ebook';
 
 class App {
   mainPageController: MainPageController;
   authController: AuthController;
+  ebook: Ebook;
 
   constructor() {
     ApiData.getDataFromLocalStorage();
     this.mainPageController = new MainPageController();
     this.authController = new AuthController();
+    this.ebook = new Ebook();
   }
 
   startApp() {
@@ -20,6 +23,8 @@ class App {
     console.log('start app');
     this.authController.checkToken();
     this.drawMainPage();
+    //this.drowContainer();
+
   }
 
   drowContainer() {
@@ -31,18 +36,35 @@ class App {
   switchToAnotherPage() {
     const nav = document.querySelector('.nav');
     const miniGames = new MiniGames();
+
+    function clearActivLink() {
+      const links = document.querySelectorAll('.nav__item');
+      links.forEach((link) => {
+        link.classList.remove('active');
+      });
+    }
+
     nav?.addEventListener('click', (e: Event) => {
       const target = e.target as HTMLLinkElement;
+      const link = target.parentNode?.parentNode as HTMLLIElement;
       const content = document.querySelector(
         '.container__content'
       ) as HTMLDivElement;
       switch (target.textContent) {
         case 'Личный кабинет':
+          content.innerHTML = '';
+          clearActivLink();
+          link.classList.add('active');
           break;
         case 'Словарь':
+          content.innerHTML = '';
+          clearActivLink();
+          link.classList.add('active');
           break;
         case 'Мини-игры':
           content.innerHTML = '';
+          clearActivLink();
+          link.classList.add('active');
           miniGames.createCarts(
             'headphones.png',
             'Аудиовызов',
@@ -58,8 +80,18 @@ class App {
           miniGames.goToStartPage();
           break;
         case 'Учебник':
+          content.innerHTML = '';
+          clearActivLink();
+          link.classList.add('active');
+          this.ebook.drawEbook();
+          break;
+        case 'Настройки':
+          content.innerHTML = '';
+          clearActivLink();
+          link.classList.add('active');
           break;
         case 'Выход':
+          content.innerHTML = '';
           break;
 
         default:
@@ -67,10 +99,10 @@ class App {
       }
     });
   }
-    
+
   drawMainPage(): void {
     this.mainPageController.getStartScreen();
-    }
+  }
 }
-    
+
 export default App;
